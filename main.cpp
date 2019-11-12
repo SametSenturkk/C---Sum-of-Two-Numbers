65b9ec59aaec135f86e8f269200478dc37078030
Samet Şentürk <senturksamet123@gmail.com>
2019-11-12 18:15:04 +0300

    C++ Programming - Sum of Two Numbers

---
+++ main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int number1,number2,total;
+	cout<<"Enter a Number1 : ";
+	cin>>number1;
+	cout<<"Enter a Number2 : ";
+	cin>>number2;
+	total=number1+number2;
+	cout<<"Total : "<<total;
+	
+	return 0;
+	
+	
+}
